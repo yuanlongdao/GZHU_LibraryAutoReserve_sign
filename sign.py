5f8da514f8f88935f48860ef667f71b61d34d8a6
ChaXxl <x734461204@gmail.com>
2023-10-07 19:21:43 +0800

    style: 调整代码注释

--- sign.py
+++ sign.py
@@ -5,10 +5,10 @@ from libs.info import infos
 from libs.source import ZWYT
 
 if __name__ == '__main__':
-    # 遍历 info 信息，获取每个用户的昵称、预约座位号、用户名、密码
+    # 遍历 info 信息，获取每个用户的昵称、预约座位号、用户名、密码、时间段、推送token（推送可以为空）
     for stu in infos:
         try:
-            # 初始化类示例，传入昵称、用户名、密码
+            # 初初始化类示例，传入昵称、用户名、密码、时间段、推送token（推送可以为空）
             yy = ZWYT(stu['name'], stu['sno'], stu['pwd'], stu['periods'], stu['pushplus'])
 
             # 调用签到函数进行签到，传入预约座位号
