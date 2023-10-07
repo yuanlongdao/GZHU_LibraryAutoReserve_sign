2fe92af1b4173c1fccdff3d794a390c92d3e4e64
ChaXxl <x734461204@gmail.com>
2023-10-07 19:01:35 +0800

    refactor: 整理代码

--- reserve.py
+++ reserve.py
@@ -6,30 +6,6 @@ from libs.info import infos
 from libs.source import ZWYT
 
 if __name__ == '__main__':
-    # action工作流
-    # 获取secrets里面的数据
-    username = os.getenv('USERNAME')
-    password = os.getenv('PASSWORD')
-    devName = os.getenv('DEVNAME')
-    periods_string = os.getenv('PERIODS')
-    pushplus = os.getenv('PUSHPLUS')
-    pushplus = pushplus if pushplus else ''
-
-    if username and password and devName and periods_string:
-        try:
-            # 初始化类示例，传入用户名、密码、时间段、推送token（推送可以为空）
-            periods = eval(periods_string)
-            yy = ZWYT('action', username, password, periods, pushplus)
-            
-            # 调用预约函数预约，传入预约座位号
-            yy.reserve(devName)  
-        except Exception as e:
-            print(e)
-            yy.pushplus("GitHub_Action预约失败", e)
-    else:
-        print("未配置GitHub Action必要参数，开始本地运行")
-
-        # 本地运行
         # 遍历 info 信息，获取每个用户的昵称、预约座位号、用户名、密码、时间段、推送token（推送可以为空）
         for stu in infos:
             try:
